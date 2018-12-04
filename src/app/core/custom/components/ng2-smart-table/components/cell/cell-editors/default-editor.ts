69a546955fbcb798a71cbc57351f5a696bbd148e
Dean Wang <dean.wang@tbwa.com>
2018-12-04 16:26:10 +0800

    #20 [Feature] (A) Smart table integration

---
+++ src/app/core/custom/components/ng2-smart-table/components/cell/cell-editors/default-editor.ts
@@ -0,0 +1,20 @@
+import { Component, Output, EventEmitter, Input } from '@angular/core';
+
+import { Cell } from '../../../lib/data-set/cell';
+
+export interface Editor {
+  cell: Cell;
+  inputClass: string;
+  onStopEditing: EventEmitter<any>;
+  onEdited: EventEmitter<any>;
+  onClick: EventEmitter<any>;
+}
+
+export class DefaultEditor implements Editor {
+  @Input() cell: Cell;
+  @Input() inputClass: string;
+
+  @Output() onStopEditing = new EventEmitter<any>();
+  @Output() onEdited = new EventEmitter<any>();
+  @Output() onClick = new EventEmitter<any>();
+}
