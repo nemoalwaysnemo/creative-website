2a4d249d432cf6497042ee3523c2ac8438de7916
Dean Wang <dean.wang@tbwa.com>
2018-12-06 16:05:39 +0800

    #24 [Feature] (A) Nuxeo JavaScript SDK test & fix

--- src/app/core/api/nuxeo/lib/base.interface.ts
+++ src/app/core/api/nuxeo/lib/base.interface.ts
@@ -27,11 +27,6 @@ export abstract class AbstractCore {
   }
 }
 
-export class LoginContext {
-  username: string;
-  password: string;
-}
-
 export class Credentials {
   clientId?: string;
   secret?: string;
@@ -58,17 +53,62 @@ export class NuxeoOptions {
   appName: string;
 }
 
-export class NuxeoOperation {
-  opts: any = {};
-  http: any;
-}
-
-export class NuxeoResponse {
-  data: any;
-}
-
 export class DocumentModel {
   id: string;
   xPath: string;
   properties: any;
+}
+
+export class NuxeoResponse {
+  private entityType: string;
+  private entity: any;
+  private entities: any;
+  private resultsCount: number;
+  private pageSize: number;
+  private currentPageSize: number;
+  private currentPageIndex: number;
+  private numberOfPages: number;
+
+  constructor(response: any = {}) {
+    this.entityType = response.entity_type;
+    this.entity = response.entity || {};
+    this.entities = response.entities || [];
+    this.resultsCount = response.resultsCount || 0;
+    this.pageSize = response.pageSize || 0;
+    this.currentPageSize = response.currentPageSize || 0;
+    this.currentPageIndex = response.currentPageIndex || 0;
+    this.numberOfPages = response.numberOfPages || 0;
+  }
+
+  getEntityType(): string {
+    return this.entityType;
+  }
+
+  getEntity(): any {
+    return this.entity;
+  }
+
+  getEntities(): any {
+    return this.entities;
+  }
+
+  getResultsCount(): number {
+    return this.resultsCount;
+  }
+
+  getPageSize(): number {
+    return this.pageSize;
+  }
+
+  getCurrentPageSize(): number {
+    return this.currentPageSize;
+  }
+
+  getCurrentPageIndex(): number {
+    return this.currentPageIndex;
+  }
+
+  getNumberOfPages(): number {
+    return this.numberOfPages;
+  }
 }
