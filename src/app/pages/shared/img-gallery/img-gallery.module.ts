36488b3101b1ca475d0dc2aaa1e4ca197dd84d9c
Nemo Yan <nemo.yan@tbwa.com>
2018-12-07 17:13:39 +0800

    #22 [Feature] (A) Image gallery (phase 2- setup)

---
+++ src/app/pages/shared/img-gallery/img-gallery.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ImgGalleryComponent } from './img-gallery.component';
+import { NgxGalleryModule } from '@core/custom/ngx-gallery'
+@NgModule({
+  imports: [
+   CommonModule,
+   NgxGalleryModule,
+  ],
+  exports: [
+    ImgGalleryComponent,
+  ],
+  declarations: [
+    ImgGalleryComponent,
+  ],
+})
+export class ImgGalleryModule {
+
+}
