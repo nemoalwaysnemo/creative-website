b589033400fc387523c4b3f6f7d949c410313949
Nemo Yan <nemo.yan@tbwa.com>
2018-12-07 17:16:35 +0800

    add_missed_imports

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,11 +3,13 @@ import { ThemeModule } from '@theme/theme.module';
 import { HomeComponent } from './home.component';
 import { HumorComponent } from './humor/humor.component';
 import { ThumbnailViewModule } from '@pages/shared/thumbnail-view/thumbnail-view.module';
+import { ImgGalleryModule } from '@pages/shared/img-gallery/img-gallery.module';
 
 @NgModule({
   imports: [
     ThemeModule,
     ThumbnailViewModule,
+    ImgGalleryModule,
   ],
   declarations: [
     HomeComponent,
