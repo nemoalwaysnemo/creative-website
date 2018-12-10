802a4d3037aae0f2ebecf957269798731ab8dedd
Dean Wang <dean.wang@tbwa.com>
2018-12-10 16:23:49 +0800

    [Update] PageProvider fetch data example

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -11,35 +11,16 @@ export class HomeComponent implements OnDestroy {
   private alive: boolean = true;
 
   constructor(private nuxeoApi: NuxeoApiService) {
-    const token = 'a636850d-1dbc-4d03-9e85-99fb7a7d36db';
+    const token = '5ee79339-b07c-408b-90cd-30484ef02688';
+    // const username = 'Administrator';
+    // const password = 'Administrator';
     this.nuxeoApi.loginWithToken(token).subscribe(response => {
       console.log(response);
-      // this.nuxeoApi.repository().schemas(['*']).fetch('/Creative').subscribe(res => {
-      //   console.log(res);
-      // });
-
-      // this.nuxeoApi.operation('Creative.RetrieveReportMetaData').schemas(['*']).params({
-      //   // 'entityType': 'acl',
-      //   // 'entityType': 'asset',
-      //   'entityType': 'contract',
-      //   'currentPageIndex': 0,
-      //   'pageSize': 10,
-      // }).execute().subscribe(res => {
-      //   console.log(res);
-      // });
 
       const restAPI = 'backslash/pp/backslash_search/execute';
       const params = {
         currentPageIndex: 1,
-        pageSize: 10,
-        active_article: true,
-        // quickFilters: 'allEdgePages',
-        void: true,
-        // the_loupe_main_prodcredits_production_date: '["thisMonth"]',
-        // tags_edges: '["Rise of the Machines"]',
-        // ecm_fulltext: 'dean',
-        // ecm_uuid: '["9af8a1cb-075e-4c93-89e3-9a85fa24d1d8"]',
-        // ecm_primaryType: '["App-Backslash-Video", "App-Backslash-Article"]',
+        pageSize: 20,
       };
 
       this.nuxeoApi.pageProvider(restAPI, params).subscribe(res => {
