cf19630ed2ecdc218c7dd7fbdafbf9ff08f3e8e7
Nemo Yan <nemo.yan@tbwa.com>
2018-12-11 15:46:15 +0800

    [Feature] (A) Image gallery (phase 3 - data source)

--- src/app/pages/shared/image-gallery/image-gallery.component.ts
+++ src/app/pages/shared/image-gallery/image-gallery.component.ts
@@ -1,17 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 import { NgxGalleryOptions, NgxGalleryImage } from '@core/custom/ngx-gallery';
-
+import { ImageGalleryDataSource } from './image-gallery-data-source.service';
 @Component({
   selector: 'tbwa-image-gallery',
   styleUrls: ['./image-gallery.component.scss'],
   template: `<ngx-gallery [options]="galleryOptions" [images]="galleryImages"></ngx-gallery>`,
 })
-export class ImageGalleryComponent implements OnInit {
+export class ImageGalleryComponent {
   galleryOptions: NgxGalleryOptions[];
   galleryImages: NgxGalleryImage[];
-  constructor() {
-  }
-  ngOnInit(): void {
+  constructor(private imageGalleryDataSource: ImageGalleryDataSource ) {
+    const params = {
+      currentPageIndex: 1,
+      pageSize: 10,
+    };
     this.galleryOptions = [
       {
         width: '600px',
@@ -19,32 +21,26 @@ export class ImageGalleryComponent implements OnInit {
         thumbnailsColumns: 4,
       },
     ];
-    this.galleryImages = [
-      {
-        small: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/1-small.jpeg',
-        medium: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/1-medium.jpeg',
-        big: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/1-big.jpeg',
-      },
-      {
-        small: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/2-small.jpeg',
-        medium: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/2-medium.jpeg',
-        big: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/2-big.jpeg',
-      },
-      {
-        small: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/3-small.jpeg',
-        medium: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/3-medium.jpeg',
-        big: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/3-big.jpeg',
-      },
-      {
-        small: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/4-small.jpeg',
-        medium: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/4-medium.jpeg',
-        big: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/4-big.jpeg',
-      },
-      {
-        small: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/5-small.jpeg',
-        medium: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/5-medium.jpeg',
-        big: 'https://lukasz-galka.github.io/ngx-gallery-demo/assets/img/5-big.jpeg',
-      },
-    ];
+    this.imageGalleryDataSource.search(params);
+    this.imageGalleryDataSource.onSearch().subscribe(res => {
+      this.galleryImages = this.getImages(res.entries);
+    });
   }
+
+
+  private getImages(entiries: any = {}){
+    var imgArray = new Array;
+    Object.keys(entiries).forEach(function (key) {
+      var thumbnailUrl = entiries[key]._contextParameters.thumbnail.url
+       var imgSetting =
+       {
+         small: thumbnailUrl,
+         medium: thumbnailUrl,
+         big: thumbnailUrl,
+       }
+      imgArray.push(imgSetting) ;
+      });
+    return imgArray;
+  };
+
 }
--- src/app/pages/shared/image-gallery/image-gallery.module.ts
+++ src/app/pages/shared/image-gallery/image-gallery.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ImageGalleryComponent } from './image-gallery.component';
 import { NgxGalleryModule } from '@core/custom/ngx-gallery';
+import { ImageGalleryDataSource } from './image-gallery-data-source.service';
 @NgModule({
   imports: [
     CommonModule,
@@ -12,6 +13,9 @@ import { NgxGalleryModule } from '@core/custom/ngx-gallery';
   ],
   declarations: [
     ImageGalleryComponent,
+  ],
+  providers: [
+    ImageGalleryDataSource,
   ],
 })
 export class ImageGalleryModule {
