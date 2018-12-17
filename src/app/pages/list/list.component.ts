e9d04d6077b5198acbccb59d5c76556e41b3c015
Jesse Dong <jesse.dong@tbwa.com>
2018-12-17 14:12:31 +0800

    feat(list page-grid view):
    #34

--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnDestroy } from '@angular/core';
+import { NuxeoApiService } from '@core/api';
 
 @Component({
   selector: 'tbwa-list-page',
@@ -9,8 +10,15 @@ export class ListComponent implements OnDestroy {
 
   private alive: boolean = true;
 
-  constructor() {
-
+  constructor(private nuxeoApi: NuxeoApiService) {
+    const token = '5ee79339-b07c-408b-90cd-30484ef02688';
+    // const username = 'Administrator';
+    // const password = 'Administrator';
+    this.nuxeoApi.loginWithToken(token).subscribe(response => {
+      console.log(response);
+    }, error => {
+      console.log(error);
+    });
   }
 
   ngOnDestroy() {
--- src/app/pages/list/list.module.ts
+++ src/app/pages/list/list.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { ThemeModule } from '@theme/theme.module';
+import { ThumbnailViewModule } from '@pages/shared/thumbnail-view/thumbnail-view.module';
 import { ListComponent } from './list.component';
+import { ListResultsComponent } from './list-results/list-results.component';
 
 @NgModule({
   imports: [
     ThemeModule,
+    ThumbnailViewModule.forRoot(),
   ],
   declarations: [
     ListComponent,
+    ListResultsComponent,
   ],
 })
 export class ListPageModule { }
