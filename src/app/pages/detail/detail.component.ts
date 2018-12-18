0cbe894f07c8953bc81b619bbb51518aa5ece2ae
Jesse Dong <jesse.dong@tbwa.com>
2018-12-18 17:04:50 +0800

    feat(detail page): check params and load document

--- src/app/pages/detail/detail.component.ts
+++ src/app/pages/detail/detail.component.ts
@@ -1,16 +1,24 @@
-import { Component, OnDestroy } from '@angular/core';
-import { NuxeoApiService } from '@core/api';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NuxeoApiService, DocumentRepository, DocumentModel } from '@core/api';
+import { Router, ActivatedRoute } from '@angular/router';
+import { takeWhile } from 'rxjs/operators';
 
 @Component({
   selector: 'tbwa-detail-page',
   styleUrls: ['./detail.component.scss'],
   templateUrl: './detail.component.html',
 })
-export class DetailComponent implements OnDestroy {
+export class DetailComponent implements OnInit, OnDestroy {
 
+  product_id: string = '24f08e3f-02d9-49a0-b07a-a8672957c7f2';
   private alive: boolean = true;
+  document: DocumentModel;
 
-  constructor(private nuxeoApi: NuxeoApiService) {
+  constructor(
+    private nuxeoApi: NuxeoApiService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private documentRepository: DocumentRepository) {
     const token = '5ee79339-b07c-408b-90cd-30484ef02688';
     // const username = 'Administrator';
     // const password = 'Administrator';
@@ -21,11 +29,36 @@ export class DetailComponent implements OnDestroy {
     });
   }
 
+  ngOnInit() {
+    // this.checkParams();
+    this.getDocument();
+  }
+
   ngOnDestroy() {
     this.alive = false;
   }
 
   nuxeoLogin() {
 
+  }
+
+  private getDocument() {
+    this.documentRepository.get(this.product_id)
+      .subscribe((res: DocumentModel) => {
+        this.document = res;
+      });
+  }
+
+
+  private checkParams(): void {
+    this.activatedRoute.queryParams
+      .pipe(
+        takeWhile(() => this.alive),
+      )
+      .subscribe(queryParams => {
+        if (!queryParams['id']) {
+          this.router.navigate(['/404']);
+        }
+      });
   }
 }
