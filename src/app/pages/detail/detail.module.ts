b89fd95f8b40d6e31d092594e566f0fe1def50f0
Jesse Dong <jesse.dong@tbwa.com>
2018-12-19 16:47:54 +0800

    feat(detail page related info): document related info component on detail page (phase 1 - tab set la
    #44

--- src/app/pages/detail/detail.module.ts
+++ src/app/pages/detail/detail.module.ts
@@ -1,21 +1,24 @@
 import { NgModule } from '@angular/core';
 import { ThemeModule } from '@theme/theme.module';
-import { NbAccordionModule } from '@core/nebular/theme';
+import { NbAccordionModule, NbTabsetModule } from '@core/nebular/theme';
 import { SharedModule } from '@pages/shared/shared.module';
 import { DetailComponent } from './detail.component';
 import { DocumentMetadataInfoComponent } from './document-metadata-info/document-metadata-info.component';
 import { DocumentAdditionalInfoComponent } from './document-additional-info/document-additional-info.component';
+import { DocumentRelatedInfoComponent } from './document-related-info/document-related-info.component';
 
 @NgModule({
   imports: [
     ThemeModule,
     SharedModule,
     NbAccordionModule,
+    NbTabsetModule,
   ],
   declarations: [
     DetailComponent,
     DocumentMetadataInfoComponent,
     DocumentAdditionalInfoComponent,
+    DocumentRelatedInfoComponent,
   ],
   providers: [
     SharedModule.forRoot().providers,
---
+++ src/app/pages/detail/document-related-info/document-related-info.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'tbwa-document-related-info',
+  styleUrls: ['./document-related-info.component.scss'],
+  templateUrl: './document-related-info.component.html',
+})
+export class DocumentRelatedInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
