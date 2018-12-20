562e0c5212291415b2898a97dedd2bf94eb007ba
Jesse Dong <jesse.dong@tbwa.com>
2018-12-20 14:59:19 +0800

    feat(detail page): document related info phase 2
    #46

--- src/app/pages/detail/document-related-info/document-related-info.component.ts
+++ src/app/pages/detail/document-related-info/document-related-info.component.ts
@@ -7,6 +7,29 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class DocumentRelatedInfoComponent implements OnInit {
 
+  tabItems = [
+    {
+      name: 'Backslash',
+      icon: 'nb-person',
+    },
+    {
+      name: 'Distruption',
+      icon: 'nb-person',
+    },
+    {
+      name: 'Knowledge',
+      icon: 'nb-person',
+    },
+    {
+      name: 'Awards',
+      icon: 'nb-person',
+    },
+    {
+      name: 'Lorern Ipsum',
+      icon: 'nb-person',
+    },
+  ];
+
   constructor() { }
 
   ngOnInit() {
