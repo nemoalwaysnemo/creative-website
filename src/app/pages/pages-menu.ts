8388eff4c3509421086d6d345d221a3930219a9f
Shaun Shang <shaun.shang@tbwa.com>
2018-12-20 10:32:10 +0800

    Modify styles for home page

--- src/app/pages/pages-menu.ts
+++ src/app/pages/pages-menu.ts
@@ -3,13 +3,13 @@ import { NbMenuItem } from '@core/nebular/theme';
 export const MENU_ITEMS: NbMenuItem[] = [
   {
     title: 'Library',
-    icon: 'nb-home',
+    icon: 'nb-creative',
     link: '/pages/home',
     home: true,
   },
   {
     title: 'Backslash',
-    icon: 'nb-home',
+    icon: 'nb-backslash',
     link: '/pages/home',
   },
 ];
