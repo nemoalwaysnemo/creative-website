bd3babe0fe8cc3d768ddc202bac18ab49ebf2c86
Dean Wang <dean.wang@tbwa.com>
2018-12-26 10:39:48 +0800

    perf(Rails env): Env for rails

---
+++ src/environments/environment.rails-dev.ts
@@ -0,0 +1,18 @@
+// This file is used for dev env
+
+export const Environment = {
+  appName: 'Creative Library Dev',
+  nuxeoUrl: 'https://library-na-dev.factory.tools/nuxeo/',
+  forgotPassword: 'https://register.adauth.me/nam/help',
+  backslashAPPUrl: '/backslash',
+  production: false,
+  autoLogin: false,
+};
+
+export const NUXEO_META_INFO = {
+  BASE_FOLDER_PATH: '/Creative',
+  AWARD_FOLDER_PATH: '/Creative/AWARDS',
+  LIBRARY_DOC_TYPES: '["App-Library-Video", "App-Library-Image"]',
+  LIBRARY_FOLDER_TYPE: '["App-Library-Folder"]',
+  LIBRARY_PROJECT_TYPE: '["App-Library-Project"]',
+};
