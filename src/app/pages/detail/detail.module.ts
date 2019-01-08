10307aabdc8bcd1a9b79b48852b1856e80eeca71
billy.zhang <billy.zhang@tbwa.com>
2019-01-08 10:39:33 +0800

    feat(docment-pdf-viewer): add component

--- src/app/pages/detail/detail.module.ts
+++ src/app/pages/detail/detail.module.ts
@@ -4,12 +4,14 @@ import { NbAccordionModule, NbTabsetModule, NbSpinnerModule } from '@core/nebula
 import { SharedModule } from '@pages/shared/shared.module';
 import { DetailComponent } from './detail.component';
 import { SimplePdfViewerModule } from 'simple-pdf-viewer';
+import { ImageViewerModule } from 'ngx-image-viewer';
 import { DocumentMetadataInfoComponent } from './document-metadata-info/document-metadata-info.component';
 import { DocumentAdditionalInfoComponent } from './document-additional-info/document-additional-info.component';
 import { DocumentRelatedInfoComponent } from './document-related-info/document-related-info.component';
 import { DocumentRelatedInfoViewComponent } from './document-related-info/document-related-info-view/document-related-info-view.component';
 import { DocumentRelatedInfoService } from './document-related-info/document-related-info.service';
 import { DocumentPdfViewerComponent } from './docment-pdf-viewer/docment-pdf-viewer.component';
+import { DocumentImageViewerComponent } from '@pages/detail/docment-image-viewer/docment-image-viewer.component';
 import { DocumentRelatedProjectComponent } from './document-related-project/document-related-project.component';
 import { DocumentRelatedAgencyComponent } from './document-related-agency/document-related-agency.component';
 
@@ -21,6 +23,7 @@ import { DocumentRelatedAgencyComponent } from './document-related-agency/docume
     NbTabsetModule,
     NbSpinnerModule,
     SimplePdfViewerModule,
+    ImageViewerModule,
   ],
   declarations: [
     DetailComponent,
@@ -29,6 +32,7 @@ import { DocumentRelatedAgencyComponent } from './document-related-agency/docume
     DocumentRelatedInfoComponent,
     DocumentRelatedInfoViewComponent,
     DocumentPdfViewerComponent,
+    DocumentImageViewerComponent,
     DocumentRelatedProjectComponent,
     DocumentRelatedAgencyComponent,
   ],
