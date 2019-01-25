8762e7b1025b07d6a6e85b2e9137f79d987235bd
Dean Wang <dean.wang@tbwa.com>
2019-01-25 09:38:29 +0800

    chore(node packages): upgrade node packages

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,22 +1,17 @@
 import { ModuleWithProviders, NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { throwIfAlreadyLoaded } from './module-import-guard';
-import { AbService } from './services/ab.service';
 import { NuxeoAuthModule } from './auth';
 import { APIModule } from './api';
 
 import {
   StateService,
   LayoutService,
-  AnalyticsService,
 } from './services';
 
 const PROVIDERS = [
-  AnalyticsService,
-  AbService,
   StateService,
   LayoutService,
-  AnalyticsService,
   APIModule.forRoot().providers,
   NuxeoAuthModule.forRoot().providers,
 ];
--- src/app/core/services/index.ts
+++ src/app/core/services/index.ts
@@ -2,4 +2,3 @@ export * from './helpers';
 export * from './state.service';
 export * from './layout.service';
 export * from './logger.service';
-export * from './analytics.service';
