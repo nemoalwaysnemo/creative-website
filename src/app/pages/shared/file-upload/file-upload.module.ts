739ef4294fef6140051710aa85a3cb72ce83a0b8
Dean Wang <dean.wang@tbwa.com>
2019-02-25 16:53:13 +0800

    feat(upload file): add a new library

--- src/app/pages/shared/file-upload/file-upload.module.ts
+++ src/app/pages/shared/file-upload/file-upload.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FileDropModule } from 'ngx-file-drop';
 import { FileUploadComponent } from './file-upload.component';
 import { BatchUploadService } from '@core/api/api.batch-upload.service';
 
 @NgModule({
   imports: [
     CommonModule,
+    FileDropModule,
   ],
   declarations: [FileUploadComponent],
   exports: [
