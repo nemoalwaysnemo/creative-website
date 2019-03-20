f65a74cf0714f50aa091393e84a16af90f36a247
Dean Wang <dean.wang@tbwa.com>
2019-03-20 15:10:40 +0800

    refactor(disruption roadmap): new disruption roadmap form

---
+++ src/app/pages/shared/disruption-form-roadmap/disruption-form-roadmap.component.ts
@@ -0,0 +1,141 @@
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { DocumentModel } from '@core/api';
+import { DynamicSuggestionModel, DynamicBatchUploadModel, DynamicInputModel } from '@core/custom';
+
+@Component({
+  selector: 'tbwa-disruption-form-roadmap',
+  styleUrls: ['./disruption-form-roadmap.component.scss'],
+  templateUrl: './disruption-form-roadmap.component.html',
+})
+export class DisruptionFormRoadmapComponent implements OnInit, OnDestroy {
+
+  private subscription: Subscription = new Subscription();
+
+  parentDocument: DocumentModel;
+
+  formLayout: any = {};
+
+  settings: any[] = [];
+
+  @Input()
+  set document(doc: DocumentModel) {
+    this.parentDocument = doc.newInstance('App-Disruption-Roadmap-Asset');
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+    this.performForm();
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  onCreated(docs: DocumentModel[]): void {
+
+  }
+
+  onUpdated(doc: DocumentModel): void {
+
+  }
+
+  private performForm(): void {
+    this.settings = this.getSettings();
+    this.formLayout = this.getFormLayout();
+  }
+
+  private getSettings(): object[] {
+    return [
+      new DynamicInputModel({
+        id: 'dc:title',
+        label: 'Title',
+        maxLength: 50,
+        placeholder: 'Title',
+        autoComplete: 'off',
+        required: false,
+        validators: {
+          required: null,
+          minLength: 4,
+        },
+        errorMessages: {
+          required: '{{label}} is required',
+          minLength: 'At least 4 characters',
+        },
+      }),
+      // new DynamicInputModel({
+      //   id: 'The_Loupe_Main:brand',
+      //   label: 'Brand',
+      //   placeholder: 'Brand',
+      //   autoComplete: 'off',
+      //   required: false,
+      // }),
+      new DynamicSuggestionModel<string>({
+        id: 'app_Edges:industry',
+        label: 'Industry',
+        directoryName: 'GLOBAL_Industries',
+        placeholder: 'Please select industry',
+      }),
+      new DynamicSuggestionModel<string>({
+        id: 'The_Loupe_Main:agency',
+        label: 'Agency',
+        directoryName: 'GLOBAL_Agencies',
+        placeholder: 'Please select agency',
+      }),
+      new DynamicSuggestionModel<string>({
+        id: 'The_Loupe_Main:country',
+        label: 'Agency Country',
+        directoryName: 'GLOBAL_Countries',
+        placeholder: 'Please select country',
+      }),
+      new DynamicSuggestionModel<string>({
+        id: 'app_Edges:Tags_edges',
+        label: 'Edges',
+        directoryName: 'App-Edges-Edges',
+        placeholder: 'Please select edges',
+      }),
+      new DynamicBatchUploadModel<string>({
+        id: 'uploadFiles',
+        label: 'Attachment',
+        formMode: 'create',
+        multiUpload: false,
+        queueLimit: 1,
+        placeholder: 'Drop file here!',
+      }),
+    ];
+  }
+
+  private getFormLayout(): any {
+    return {
+      'dc:title': {
+        element: {
+          container: 'p-0',
+          label: 'col-form-label',
+        },
+        grid: {
+          host: 'col-sm-4',
+        },
+      },
+      'The_Loupe_Main:brand': {
+        element: {
+          container: 'p-0',
+          label: 'col-form-label',
+        },
+        grid: {
+          host: 'col-sm-4',
+        },
+      },
+      'app_Edges:industry': {
+        element: {
+          container: 'p-0',
+          label: 'col-form-label',
+        },
+        grid: {
+          host: 'col-sm-4',
+        },
+      },
+    };
+  }
+
+}
