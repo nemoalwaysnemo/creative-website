bd980be61b27acfbb96711b935c693cb3daed292
Shaun Shang <shaun.shang@tbwa.com>
2019-04-18 12:26:23 +0800

    Modify styles for intelligence page

--- src/app/pages/disruption/disruption-folders-view/disruption-folders-view.component.ts
+++ src/app/pages/disruption/disruption-folders-view/disruption-folders-view.component.ts
@@ -2,7 +2,7 @@ import { Component, Input, ChangeDetectionStrategy} from '@angular/core';
 
 @Component({
   selector: 'disruption-folders-view',
-  styleUrls: ['./disruption-folders-view.component.scss'],
+  styleUrls: ['../../../theme/styles/disruption-folders.scss'],
   templateUrl: './disruption-folders-view.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
--- src/app/pages/intelligence/intelligence-shared/intelligence-folders-view/intelligence-folders-view.component.ts
+++ src/app/pages/intelligence/intelligence-shared/intelligence-folders-view/intelligence-folders-view.component.ts
@@ -2,7 +2,7 @@ import { Component, Input, ChangeDetectionStrategy} from '@angular/core';
 
 @Component({
   selector: 'intelligence-folders-view',
-  styleUrls: ['./intelligence-folders-view.component.scss'],
+  styleUrls: ['../../../../theme/styles/disruption-folders.scss'],
   templateUrl: './intelligence-folders-view.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
