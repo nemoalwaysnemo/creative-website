cc096c2b9336909cd69c3307f8b229954fcd8b09
nemo.yan <nemo.yan@tbwa.com>
2019-05-15 09:41:52 +0800

    feat(disruption folder): disruption folder view
    add link redirect for disruption folder view
    
    re #257

--- src/app/pages/disruption/disruption-folders-view/disruption-folders-view.module.ts
+++ src/app/pages/disruption/disruption-folders-view/disruption-folders-view.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { NbSpinnerModule } from '@core/nebular/theme';
 import { DisruptionFoldersViewComponent } from './disruption-folders-view.component';
 import { PreviewDialogModule, DisruptionFormDayModule } from '@pages/shared';
@@ -8,6 +9,7 @@ import { DisruptionFormFoldersEditDialogComponent } from './disruption-folders-f
 @NgModule({
   imports: [
     CommonModule,
+    RouterModule,
     NbSpinnerModule,
     PreviewDialogModule,
     DisruptionFormDayModule,
