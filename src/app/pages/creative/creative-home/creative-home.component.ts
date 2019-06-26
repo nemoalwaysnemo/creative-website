3babf7eb33d4c711173c19916f2a926767849111
nemo.yan <nemo.yan@tbwa.com>
2019-06-26 11:12:44 +0800

    feat(new wheel animation): add new mouse wheel scroll animation
    re #280

--- src/app/pages/creative/creative-home/creative-home.component.ts
+++ src/app/pages/creative/creative-home/creative-home.component.ts
@@ -1,6 +1,8 @@
 import { NUXEO_PATH_INFO, NUXEO_META_INFO } from '@environment/environment';
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, HostListener } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { NbLayoutScrollService } from '@core/nebular/theme/services/scroll.service.ts';
+import { debounce } from './debounce';
 
 @Component({
   selector: 'creative-home-page',
@@ -9,6 +11,7 @@ import { Subscription } from 'rxjs';
 })
 export class CreativeHomeComponent implements OnInit, OnDestroy {
 
+  private destination: number = 0;
   headline: string = 'This is how we kill boring.';
 
   subHead: string = 'Our entire collection of disruptive work is all right here.';
@@ -23,7 +26,19 @@ export class CreativeHomeComponent implements OnInit, OnDestroy {
     ecm_primaryType: NUXEO_META_INFO.CREATIVE_IMAGE_VIDEO_AUDIO_TYPES,
   };
 
-  constructor() { }
+  constructor(private scrollService: NbLayoutScrollService) { }
+  @HostListener('mousewheel', ['$event'])
+  @debounce()
+  scroll(event: MouseEvent) {
+    const delta = event['deltaY'];
+    if (delta > 0 && this.destination < 3) {
+      this.destination = this.destination + 1;
+      this.scrollService.triggerScrollTo('destination-' + this.destination);
+    } else if (delta < 0 && this.destination > 0) {
+      this.destination = this.destination - 1;
+      this.scrollService.triggerScrollTo('destination-' + this.destination);
+    }
+  }
 
   ngOnInit() {
   }
---
+++ src/app/pages/creative/creative-home/debounce.ts
@@ -0,0 +1,11 @@
+export function debounce(delay: number = 200): MethodDecorator {
+  return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    let timeout = null;
+    const original = descriptor.value;
+    descriptor.value = function (...args) {
+      clearTimeout(timeout);
+      timeout = setTimeout(() => original.apply(this, args), delay);
+    };
+    return descriptor;
+  };
+}
