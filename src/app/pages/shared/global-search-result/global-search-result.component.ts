49b24646127968887d978cbf317414de0284b657
Dean Wang <dean.wang@tbwa.com>
2019-07-09 13:37:24 +0800

    refactor(global search result): code refactoring

--- src/app/pages/shared/global-search-result/global-search-result.component.ts
+++ src/app/pages/shared/global-search-result/global-search-result.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy, Input, TemplateRef } from '@angular/core';
-import { AdvanceSearch } from '@core/api';
+import { AdvanceSearch, SearchResponse } from '@core/api';
 import { SearchQueryParamsService } from '../services/search-query-params.service';
 import { BaseSearchResultComponent } from './abstract-search-result';
 
@@ -10,11 +10,21 @@ import { BaseSearchResultComponent } from './abstract-search-result';
 })
 export class GlobalSearchResultComponent extends BaseSearchResultComponent implements OnInit, OnDestroy {
 
+  @Input() responseHandler: Function = (res: SearchResponse): SearchResponse => res;
+
   @Input() templateRef: TemplateRef<any>;
+
+  @Input() hasPagination: boolean = true;
+
   @Input() layout: string = 'quarter';
 
   constructor(protected advanceSearch: AdvanceSearch, protected queryParamsService: SearchQueryParamsService) {
     super(advanceSearch, queryParamsService);
+  }
+
+  protected handleResponse(res: SearchResponse): void {
+    const response = this.responseHandler(res);
+    super.handleResponse(response);
   }
 
 }
