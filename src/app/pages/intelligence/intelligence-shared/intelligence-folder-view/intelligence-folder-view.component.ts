9e6e5db0f2ee54c45c5b656010403993f6d7a353
emily.zhou@tbwa.com <emily.zhou@tbwa.com>
2019-07-15 11:30:02 +0800

    refactor(intelligence back button): intelligence back button
    re #297

--- src/app/pages/intelligence/intelligence-shared/intelligence-folder-view/intelligence-folder-view.component.ts
+++ src/app/pages/intelligence/intelligence-shared/intelligence-folder-view/intelligence-folder-view.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input, ChangeDetectionStrategy} from '@angular/core';
+import { NUXEO_PATH_INFO } from '@environment/environment';
+import { Router } from '@angular/router';
+import { DocumentModel } from '@core/api';
 
 @Component({
   selector: 'intelligence-folder-view',
@@ -10,6 +13,28 @@ export class IntelligenceFolderViewComponent {
 
   @Input() loading: boolean;
 
-  @Input() document: any;
+  @Input() doc: DocumentModel;
+
+  @Input() set document(doc: DocumentModel) {
+    if (doc) {
+      this.doc = doc;
+    }
+  }
+
+  constructor(
+    private router: Router,
+  ) { }
+
+  backToParent(): void {
+    const rootPath: string = NUXEO_PATH_INFO.INTELLIGENCE_BASE_FOLDER_PATH;
+    const splitPath: string = this.doc.path.split(rootPath)[1];
+    const childSplitPath: Array<string> = splitPath.split('/');
+
+    if ( childSplitPath.length < 2 ) {
+      this.router.navigate(['p/redirect'], { queryParams: { url: `/p/intelligence/` } });
+    } else {
+      this.router.navigate(['p/redirect'], { queryParams: { url: `/p/intelligence/folder/${this.doc.parentRef}` } });
+    }
+  }
 
 }
