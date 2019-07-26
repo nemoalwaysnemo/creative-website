fafd899c0285ffb3f6784b38f94f6c5e2801ddde
nemo.yan <nemo.yan@tbwa.com>
2019-07-26 11:10:42 +0800

    refactor(section scroll): remove section scroll function
    re #313

--- src/app/pages/creative/creative-home/creative-home.component.ts
+++ src/app/pages/creative/creative-home/creative-home.component.ts
@@ -1,17 +1,13 @@
 import { NUXEO_PATH_INFO, NUXEO_META_INFO } from '@environment/environment';
-import { Component, OnInit, OnDestroy, HostListener } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
-import { NbLayoutScrollService } from '@core/nebular/theme/services/scroll.service.ts';
-import { debounce } from './debounce';
 
 @Component({
   selector: 'creative-home-page',
   styleUrls: ['./creative-home.component.scss'],
   templateUrl: './creative-home.component.html',
 })
-export class CreativeHomeComponent implements OnInit, OnDestroy {
-
-  private destination: number = 0;
+export class CreativeHomeComponent implements OnDestroy {
 
   headline: string = 'This is how we kill boring.';
 
@@ -42,37 +38,9 @@ export class CreativeHomeComponent implements OnInit, OnDestroy {
     ecm_fulltext: '',
   };
 
-  constructor(private scrollService: NbLayoutScrollService) { }
-  @HostListener('mousewheel', ['$event'])
-  @debounce()
-  scroll(event: MouseEvent) {
-    const delta = event['deltaY'];
-    if (this.scrollToglle) {
-      if (delta > 0 && this.destination < 3) {
-        this.destination = this.destination + 1;
-      } else if (delta < 0 && this.destination > 0) {
-        this.destination = this.destination - 1;
-      }
-      this.scrollToSection(this.destination);
-    }
-  }
-
-  ngOnInit() {
-    this.scrollService.onSectionScroll()
-      .subscribe((res) => {
-        this.scrollToglle = res.tag;
-      });
-  }
+  constructor() { }
 
   ngOnDestroy() {
     this.subscription.unsubscribe();
-  }
-
-  private scrollToSection(destination) {
-    if (destination === 2 || destination === 3) {
-      this.scrollService.triggerScrollTo('destination-' + destination, -150);
-    } else {
-      this.scrollService.triggerScrollTo('destination-' + destination);
-    }
   }
 }
