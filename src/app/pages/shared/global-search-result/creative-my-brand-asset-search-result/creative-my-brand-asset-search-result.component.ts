e36a6b4bbec9467de7fbbce2eea08559998c8c2f
emily.zhou@tbwa.com <emily.zhou@tbwa.com>
2019-07-30 14:19:00 +0800

    refactor(optimize properties title): optimize properties title

--- src/app/pages/shared/global-search-result/creative-my-brand-asset-search-result/creative-my-brand-asset-search-result.component.ts
+++ src/app/pages/shared/global-search-result/creative-my-brand-asset-search-result/creative-my-brand-asset-search-result.component.ts
@@ -36,7 +36,7 @@ export class CreativeMyBrandAssetSearchResultComponent {
         uid: doc.uid,
         title: doc.title,
         productionDate: doc.get('The_Loupe_ProdCredits:production_date'),
-        campaign: doc.get('The_Loupe_Main:campaign_title'),
+        campaign: doc.get('The_Loupe_Main:campaign_title_'),
         ceativeDirector: doc.get('The_Loupe_Credits:creativeDirector'),
         artDirector: doc.get('The_Loupe_Credits:artProducer'),
         producer: doc.get('The_Loupe_Credits:producer'),
