936625171b520413f2650746beeac2f3d8898ec3
nemo.yan <nemo.yan@tbwa.com>
2019-08-08 14:08:11 +0800

    refactor(missed file): commit missed file

--- src/app/pages/shared/global-search-result/global-search-result.component.ts
+++ src/app/pages/shared/global-search-result/global-search-result.component.ts
@@ -2,7 +2,7 @@ import { Component, Input, TemplateRef } from '@angular/core';
 import { AdvanceSearch } from '@core/api';
 import { SearchQueryParamsService } from '../services/search-query-params.service';
 import { AbstractGlobalSearchResultComponent } from './abstract-global-search-result.component';
-
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'global-search-result',
   styleUrls: ['./global-search-result.component.scss'],
@@ -16,8 +16,8 @@ export class GlobalSearchResultComponent extends AbstractGlobalSearchResultCompo
 
   @Input() layout: string = 'quarter';
 
-  constructor(protected advanceSearch: AdvanceSearch, protected queryParamsService: SearchQueryParamsService) {
-    super(advanceSearch, queryParamsService);
+  constructor(protected advanceSearch: AdvanceSearch, protected queryParamsService: SearchQueryParamsService, protected route: ActivatedRoute) {
+    super(advanceSearch, queryParamsService, route);
   }
 
 }
