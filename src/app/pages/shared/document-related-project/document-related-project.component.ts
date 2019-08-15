42eec62e9680d2fc4864c7e0575d4f68e8b6513c
Dean Wang <dean.wang@tbwa.com>
2019-08-16 07:58:09 +0800

    fix(see more brands): change ecm path to tbwa
    re #358

--- src/app/pages/shared/document-related-project/document-related-project.component.ts
+++ src/app/pages/shared/document-related-project/document-related-project.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnDestroy } from '@angular/core';
 import { AdvanceSearch, NuxeoPagination, DocumentModel } from '@core/api';
-import { NUXEO_META_INFO } from '@environment/environment';
+import { NUXEO_META_INFO, NUXEO_PATH_INFO } from '@environment/environment';
 import { Subscription } from 'rxjs';
 
 @Component({
@@ -23,6 +23,7 @@ export class DocumentRelatedProjectComponent implements OnDestroy {
   private params: any = {
     pageSize: 4,
     ecm_primaryType: NUXEO_META_INFO.CREATIVE_IMAGE_VIDEO_TYPES,
+    ecm_path: NUXEO_PATH_INFO.CREATIVE_BASE_FOLDER_PATH + 'TBWA-',
     the_loupe_main_brand_any: '',
     ecm_uuid_not_eq: '',
   };
