24c3ffc276681745f042ae73014a51c11dcafedb
emily.zhou@tbwa.com <emily.zhou@tbwa.com>
2019-08-26 17:34:26 +0800

    refactor(popular brand filter add year): popular brand filter add year
    re #360

--- src/app/pages/global-search/creative-search/creative-popular-brand-asset-search/creative-popular-brand-asset-search.component.ts
+++ src/app/pages/global-search/creative-search/creative-popular-brand-asset-search/creative-popular-brand-asset-search.component.ts
@@ -22,6 +22,7 @@ export class CreativePopularBrandAssetSearchComponent extends AbstractDocumentVi
     new SearchFilterModel({ key: 'the_loupe_main_assettype_agg', placeholder: 'Asset Type' }),
     new SearchFilterModel({ key: 'app_edges_tags_edges_agg', placeholder: 'Edges' }),
     new SearchFilterModel({ key: 'the_loupe_main_campaign_agg', placeholder: 'Campaign', convertTitle: true }),
+    new SearchFilterModel({ key: 'the_loupe_prodCredits_production_date_agg', placeholder: 'Year' }),
     new SearchFilterModel({ key: 'app_global_networkshare_agg', placeholder: 'Showcase', optionLabels: { 'true': 'Yes', 'false': 'No' } }),
   ];
 
