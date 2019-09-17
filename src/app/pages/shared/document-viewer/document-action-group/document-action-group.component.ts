29d31b4ae4095910b3c2400430470cc22b8aced4
Dean Wang <dean.wang@tbwa.com>
2019-09-17 14:57:15 +0800

    feat(document action group): new widget for buttons

---
+++ src/app/pages/shared/document-viewer/document-action-group/document-action-group.component.ts
@@ -0,0 +1,57 @@
+import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
+import { Location } from '@angular/common';
+import { Observable, of as observableOf } from 'rxjs';
+import { concatMap, map, share } from 'rxjs/operators';
+import { DocumentModel, UserService, UserModel, NuxeoUserGroups } from '@core/api';
+import { getDocumentTypes } from '@core/services';
+import { NUXEO_META_INFO } from '@environment/environment';
+
+@Component({
+  selector: 'document-action-group',
+  styleUrls: ['./document-action-group.component.scss'],
+  templateUrl: './document-action-group.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DocumentActionGroupComponent {
+
+  documentModel: DocumentModel;
+
+  downloadPermission$: Observable<boolean> = observableOf(false);
+
+  @Input() styleName: string;
+
+  @Input()
+  set document(doc: DocumentModel) {
+    if (doc) {
+      this.documentModel = doc;
+      if (this.isCreativeAsset(doc)) {
+        this.downloadPermission$ = this.canDownloadCreativeAsset(doc);
+      } else {
+        this.downloadPermission$ = observableOf(true);
+      }
+    }
+  }
+
+  constructor(
+    private userService: UserService,
+    private location: Location,
+  ) { }
+
+  isCreativeAsset(doc: DocumentModel): boolean {
+    return doc && getDocumentTypes(NUXEO_META_INFO.CREATIVE_IMAGE_VIDEO_AUDIO_TYPES).includes(doc.type);
+  }
+
+  canDownloadCreativeAsset(doc: DocumentModel): Observable<boolean> {
+    return this.userService.getCurrentUserInfo().pipe(
+      concatMap((user: UserModel) => doc.getParentProperty('app_global:download_mainfile').pipe(
+        map((permission: boolean) => user.hasGroup(NuxeoUserGroups.Everyone) && permission === true),
+      )),
+      share(),
+    );
+  }
+
+  goBack() {
+    this.location.back();
+  }
+
+}
