96ad500471c350e7bf13cbf9c75adaa0b24739ff
Dean Wang <dean.wang@tbwa.com>
2020-01-13 13:42:22 +0800

    feat(auto redirection): redirect to requested url

--- src/app/core/base-auth/services/auth-guard.service.ts
+++ src/app/core/base-auth/services/auth-guard.service.ts
@@ -1,22 +1,35 @@
 import { Injectable } from '@angular/core';
+import { CanActivate, Router, RouterStateSnapshot, ActivatedRouteSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
-import { CanActivate, Router } from '@angular/router';
 import { NbAuthService } from './auth.service';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
 
-  constructor(private authService: NbAuthService, private router: Router) {
+  constructor(private authService: NbAuthService, private router: Router, private cookieService: CookieService) {
   }
 
-  canActivate() {
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
     return this.authService.isAuthenticated()
       .pipe(
         tap(authenticated => {
           if (!authenticated) {
+            if (!this.getRequestedUrl()) {
+              this.setRequestedUrl(state.url);
+            }
             this.router.navigate(['/auth/login']);
           }
         }),
       );
+  }
+
+  private setRequestedUrl(url: string): void {
+    return this.cookieService.set('requestedUrl', url, 3600, '/');
+  }
+
+  private getRequestedUrl(): string {
+    return this.cookieService.get('requestedUrl');
   }
 }
