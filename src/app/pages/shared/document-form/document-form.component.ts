d6d8a68c59f653aca467e0cabbbfe068cab0f2bb
billy.zhang <billy.zhang@tbwa.com>
2020-01-14 08:42:01 +0800

    fix(document form): bug fxi

--- src/app/pages/shared/document-form/document-form.component.ts
+++ src/app/pages/shared/document-form/document-form.component.ts
@@ -178,6 +178,12 @@ export class DocumentFormComponent implements OnInit, OnChanges, OnDestroy {
       documents = this.attachFiles(this.documentModel, this.formGroup.value[this.uploadFieldName]);
     } else {
       documents = [this.documentModel];
+      documents.forEach(doc => {
+        if (doc.properties['files:files'] === null) {
+          delete doc.properties['files:files'];
+        }
+      });
+
     }
     this.createDocuments(documents).subscribe((models: DocumentModel[]) => {
       this.onCreated.next(models);
