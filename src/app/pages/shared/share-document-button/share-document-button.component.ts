f41800a72834ab30e45ed21397a5d74d4dea20fb
Dean Wang <dean.wang@tbwa.com>
2020-04-07 16:04:35 +0800

    perf(share button): code improvement

--- src/app/pages/shared/share-document-button/share-document-button.component.ts
+++ src/app/pages/shared/share-document-button/share-document-button.component.ts
@@ -1,31 +1,32 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'share-document-button',
   templateUrl: './share-document-button.component.html',
   styleUrls: ['./share-document-button.component.scss'],
 })
-export class ShareDocumentButtonComponent implements OnInit {
-
-  constructor() {}
+export class ShareDocumentButtonComponent {
 
   @Input() currentUrl: string = window.location.href;
 
   btn: string = 'Copy';
 
-  ngOnInit() {}
-
   onCopy(event: any): void {
     this.btn = 'Done';
     if (event.isSuccess === true) {
-      setTimeout(() => { this.btn = 'Copy'; }, 2000);
+      timer(2000).subscribe(() => { this.btn = 'Copy'; });
     }
   }
 
   onShareGroup(): void {
-    const url = 'https://work.facebook.com/sharer.php?display=popup&u=' + encodeURIComponent(this.currentUrl);
+    const url = 'https://work.facebook.com/sharer.php?display=popup&u=' + this.getShareUrl();
     const options = 'toolbar=0,status=0,resizable=1,width=626,height=436';
     window.open(url, 'sharer', options);
+  }
+
+  private getShareUrl(): string {
+    return encodeURIComponent(this.currentUrl);
   }
 
 }
