64dec002c52f1ac8e658f666abbad544c47d1c51
Emily Zhou <emily.zhou@tbwa.com>
2020-04-07 14:17:20 +0800

    refactor(share asset to workplace): share asset to workplace
    share asset to workplace in a group
    
    re #445

--- src/app/pages/shared/share-document-button/share-document-button.component.ts
+++ src/app/pages/shared/share-document-button/share-document-button.component.ts
@@ -22,4 +22,10 @@ export class ShareDocumentButtonComponent implements OnInit {
     }
   }
 
+  onShareGroup(): void {
+    const url = 'https://work.facebook.com/sharer.php?display=popup&u=' + encodeURIComponent(this.currentUrl);
+    const options = 'toolbar=0,status=0,resizable=1,width=626,height=436';
+    window.open(url, 'sharer', options);
+  }
+
 }
