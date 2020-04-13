6bb233364a2001b55602d70e1a6793f783548394
Dean Wang <dean.wang@tbwa.com>
2020-04-13 12:24:19 +0800

    feat(favorite): favorite module

---
+++ src/app/pages/favorite/abstract-favorite-document-view.component.ts
@@ -0,0 +1,47 @@
+import { OnInit } from '@angular/core';
+import { DocumentModel, AdvanceSearch, UserService } from '@core/api';
+import { tap } from 'rxjs/operators';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { PreviewDialogService, SearchQueryParamsService, AbstractDocumentViewComponent } from '@pages/shared';
+
+export abstract class AbstractFavoriteDocumentViewComponent extends AbstractDocumentViewComponent implements OnInit {
+
+  baseParams$: BehaviorSubject<any> = new BehaviorSubject<any>(null);
+
+  constructor(
+    protected advanceSearch: AdvanceSearch,
+    protected activatedRoute: ActivatedRoute,
+    protected queryParamsService: SearchQueryParamsService,
+    protected previewDialogService: PreviewDialogService,
+    protected userService: UserService) {
+    super(advanceSearch, activatedRoute, queryParamsService);
+  }
+
+  ngOnInit() {
+    const subscription = this.getFavoriteDocument().subscribe();
+    this.subscription.add(subscription);
+  }
+
+  protected getCurrentDocumentSearchParams(): object {
+    return {};
+  }
+
+  protected setCurrentDocument(doc: DocumentModel) {
+    if (doc) {
+      this.baseParams$.next(this.buildAssetsParams(doc));
+    }
+  }
+
+  protected getFavoriteDocument(): Observable<DocumentModel> {
+    return this.userService.getFavoriteDocument().pipe(
+      tap((doc: DocumentModel) => {
+        this.loading = false;
+        this.setCurrentDocument(doc);
+      }),
+    );
+  }
+
+  protected abstract buildAssetsParams(doc: DocumentModel): object;
+
+}
