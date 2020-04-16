e5e395a85acb15df650c2b0b77c8d543198180d5
Dean Wang <dean.wang@tbwa.com>
2020-04-16 15:05:29 +0800

    improvement(theme): code improvement

--- src/app/pages/shared/global-search-result/global-search-result.component.ts
+++ src/app/pages/shared/global-search-result/global-search-result.component.ts
@@ -2,8 +2,7 @@ import { Component, Input, TemplateRef } from '@angular/core';
 import { AdvanceSearch } from '@core/api';
 import { SearchQueryParamsService } from '../services/search-query-params.service';
 import { AbstractGlobalSearchResultComponent } from './abstract-global-search-result.component';
-import { ActivatedRoute } from '@angular/router';
-import { ViewportScroller } from '@angular/common';
+
 @Component({
   selector: 'global-search-result',
   styleUrls: ['./global-search-result.component.scss'],
