55872a125c5a089e04d264a4fe41db64f02d553c
Dean Wang <dean.wang@tbwa.com>
2020-04-20 11:29:30 +0800

    refactor(provider): code refactoring

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BaseAuthModule } from './base-auth';
+import { NgxPermissionsModule } from 'ngx-permissions';
+import { DeviceDetectorModule } from 'ngx-device-detector';
 import { APIModule } from './api';
 
 @NgModule({
   imports: [
     APIModule,
     BaseAuthModule,
+    NgxPermissionsModule.forRoot(),
+    DeviceDetectorModule.forRoot(),
+  ],
+  exports: [
+    NgxPermissionsModule,
+    DeviceDetectorModule,
   ],
 })
 export class CoreModule {
