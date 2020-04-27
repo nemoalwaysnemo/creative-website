986da61c13c7ef43cee225581ebbff368427baa7
ralhanvansh <ralhanvansh5@gmail.com>
2020-04-27 11:33:26 +0530

    refactor(biz-dev): leadearship asset search result remove commented code

--- src/app/pages/shared/global-search-result/biz-dev-thought-leadership-asset-search-result/biz-dev-thought-leadership-asset-search-result.component.ts
+++ src/app/pages/shared/global-search-result/biz-dev-thought-leadership-asset-search-result/biz-dev-thought-leadership-asset-search-result.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { AbstractSearchResultComponent } from '../abstract-search-result.component';
 import { SearchQueryParamsService } from '../../services/search-query-params.service';
-// import { Router } from '@angular/router';
-// import { DocumentModel } from '@core/api';
 
 @Component({
   selector: 'biz-dev-thought-leadership-asset-search-result',
@@ -11,24 +9,12 @@ import { SearchQueryParamsService } from '../../services/search-query-params.ser
 })
 export class BizDevThoughtLeadershipAssetSearchResultComponent extends AbstractSearchResultComponent {
 
-  constructor(
-    protected queryParamsService: SearchQueryParamsService,
-    // private router: Router
-  ) {
+  constructor(protected queryParamsService: SearchQueryParamsService) {
     super(queryParamsService);
   }
 
   protected onInit(): void {
     this.onQueryParamsChanged();
   }
-
-  // private navigateToDocument(doc?: DocumentModel) {
-  //   if (doc.type === 'App-BizDev-Thought-Folder') {
-  //     this.router.navigate(['/p/business-development/Thought Leadership/folder/', doc.uid], { queryParams: this.queryParams});
-  //
-  //   } else {
-  //     this.router.navigate(['/p/business-development/asset/', doc.uid], { queryParams: this.queryParams});
-  //   }
-  // }
 
 }
