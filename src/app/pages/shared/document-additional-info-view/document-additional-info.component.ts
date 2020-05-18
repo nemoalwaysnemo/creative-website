58cc720aab6a27a75238f9e90ca54d9088859589
ralhanvansh <ralhanvansh5@gmail.com>
2020-05-18 15:53:24 +0530

    feat(biz-dev): biz-dev asset page additional attachments display

--- src/app/pages/shared/document-additional-info-view/document-additional-info.component.ts
+++ src/app/pages/shared/document-additional-info-view/document-additional-info.component.ts
@@ -1,6 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, TemplateRef } from '@angular/core';
 import { DocumentModel } from '@core/api';
 import { NUXEO_META_INFO } from '@environment/environment';
+import { GlobalDocumentDialogService } from '../global-document-dialog/global-document-dialog.service';
+import { getDocumentTypes } from '@core/services/helpers';
+import { GLOBAL_DOCUMENT_DIALOG } from '../global-document-dialog';
+
 
 @Component({
   selector: 'document-additional-info',
@@ -15,12 +19,33 @@ export class DocumentAdditionalInfoComponent implements OnInit {
 
   attachments: { type: any, url: any, title: any }[] = [];
 
+  generalComponent: any = GLOBAL_DOCUMENT_DIALOG.GENERAL_DOWNLOAD_REQUEST;
+
+  constructor(
+    private globalDocumentDialogService: GlobalDocumentDialogService,
+  ) { }
+
   ngOnInit() {
     if (NUXEO_META_INFO.DISRUPTION_ASSET_TYPE.includes(this.document.type)) {
       this.docType = 'Disruption';
       this.attachments = this.document.getAttachmentList();
+    } else if (NUXEO_META_INFO.BIZ_DEV_ASSET_TYPE.includes(this.document.type)) {
+      this.docType = 'Business-Development';
+      this.attachments = this.document.getAttachmentList();
     } else if (NUXEO_META_INFO.CREATIVE_IMAGE_VIDEO_AUDIO_TYPES.includes(this.document.type)) {
       this.docType = 'Creative';
     }
+  }
+
+  private openDialog(dialog: TemplateRef<any>): void {
+    this.globalDocumentDialogService.open(dialog);
+  }
+
+  private isNeedSendDownloadRequest(doc: DocumentModel): boolean {
+    return this.isBizDevCaseStudyAsset(doc) && doc.get('app_global:asset_request') === true;
+  }
+
+  private isBizDevCaseStudyAsset(doc: DocumentModel): boolean {
+    return doc && getDocumentTypes(NUXEO_META_INFO.BIZ_DEV_CASE_STUDIES_ASSET_TYPE).includes(doc.type);
   }
 }
--- src/app/pages/shared/document-additional-info-view/document-additional-info.module.ts
+++ src/app/pages/shared/document-additional-info-view/document-additional-info.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ThemeModule } from '@theme/theme.module';
 import { DocumentAdditionalInfoComponent } from './document-additional-info.component';
+import { GlobalDocumentDialogModule } from '../global-document-dialog/global-document-dialog.module';
+
 
 @NgModule({
   imports: [
     CommonModule,
     ThemeModule,
+    GlobalDocumentDialogModule,
   ],
   declarations: [
     DocumentAdditionalInfoComponent,
