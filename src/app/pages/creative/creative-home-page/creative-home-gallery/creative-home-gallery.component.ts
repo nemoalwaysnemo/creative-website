4233a0ceb1581f4ef2d4b0492519be5ae9651b8d
Emily Zhou <emily.zhou@tbwa.com>
2020-06-08 09:17:36 +0800

    refactor(creative carousel popup): creative carousel popup
    re #520

--- src/app/pages/creative/creative-home-page/creative-home-gallery/creative-home-gallery.component.ts
+++ src/app/pages/creative/creative-home-page/creative-home-gallery/creative-home-gallery.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { NuxeoPagination, DocumentModel } from '@core/api';
 import { DocumentPageService } from '@pages/shared';
-import { NUXEO_PATH_INFO, NUXEO_DOC_TYPE } from '@environment/environment';
+import { NUXEO_PATH_INFO, NUXEO_DOC_TYPE, Environment } from '@environment/environment';
+import { parseCountry } from '@core/services/helpers';
 
 @Component({
   selector: 'creative-home-gallery',
@@ -20,6 +21,18 @@ export class CreativeHomeGalleryComponent implements OnInit, OnDestroy {
     loop: true,
     thumb: false,
   };
+
+  galleryEvent: string = 'play';
+
+  status: string = 'closed';
+
+  showInfo: boolean = false;
+
+  playStatus: boolean = false;
+
+  document: DocumentModel;
+
+  currentUrl: string;
 
   private params: any = {
     pageSize: 10,
@@ -42,21 +55,72 @@ export class CreativeHomeGalleryComponent implements OnInit, OnDestroy {
     this.subscription.unsubscribe();
   }
 
+  onPlayingChange(e: any): void {
+    this.playStatus = (e && e.isPlaying === true) ? false : true;
+    this.showInfo = this.showInfo && this.playStatus;
+    this.onStatusChanged();
+    this.toggleStatus();
+  }
+
+  onStatusChanged(): void {
+    this.galleryEvent = this.showInfo === true ? 'stop' : 'play';
+  }
+
+  toggleStatus(): void {
+    this.status = this.showInfo === true ? 'opened' : 'closed';
+  }
+
   private getItems(entiries: DocumentModel[]) {
     const imgArray = new Array();
-    for (const entry of entiries) {
-      if (entry.isVideo() && this.hasVideoContent(entry)) {
-        imgArray.push({ src: entry.getCarouselVideoSources(), thumb: entry.attachedImage, poster: entry.attachedImage, title: entry.title, uid: entry.uid, description: entry.get('dc:description') });
-      } else if (entry.isPicture()) {
-        const url = entry.attachedImage;
-        imgArray.push({ src: url, thumb: url, title: entry.title, uid: entry.uid, description: entry.get('dc:description') });
+    for (const doc of entiries) {
+      if (doc.isVideo() && this.hasVideoContent(doc)) {
+        imgArray.push({ src: doc.getCarouselVideoSources(), thumb: doc.attachedImage, poster: doc.attachedImage, title: doc.title, uid: doc.uid, description: doc.get('dc:description'), doc });
+      } else if (doc.isPicture()) {
+        const url = doc.attachedImage;
+        imgArray.push({ src: url, thumb: url, title: doc.title, uid: doc.uid, description: doc.get('dc:description'), doc });
       } else {
       }
     }
     return imgArray;
   }
 
-  hasVideoContent(entry: DocumentModel) {
-    return entry.getVideoSources().length > 0;
+  hasVideoContent(doc: DocumentModel) {
+    return doc.getVideoSources().length > 0;
+  }
+
+  toggleInfo(doc: DocumentModel): void {
+    this.showInfo = !this.showInfo;
+    this.onStatusChanged();
+    this.toggleStatus();
+    this.document = doc;
+    if (this.showInfo) {
+      this.currentUrl = this.buildShareUrl(doc.uid);
+      this.documentPageService.googleAnalyticsTrackEvent({
+        'event_category': 'Gallery',
+        'event_action': `Gallery Item Preview`,
+        'event_label': `Gallery Item Preview - ${doc.title}`,
+        'event_value': doc.uid,
+        'dimensions.docId': doc.uid,
+        'dimensions.docTitle': doc.title,
+      });
+    }
+  }
+
+  previewBtnImage(): string {
+    return this.assetPath('assets/images/preview_logo.png');
+  }
+
+  parseCountry(list: string[]) {
+    return parseCountry(list);
+  }
+
+  protected assetPath(src: string): string {
+    return Environment.assetPath + src;
+  }
+
+  private buildShareUrl(uid: string): string {
+    this.currentUrl = window.location.href;
+    const shareUrl = '/asset/' + uid;
+    return this.currentUrl.indexOf('/home') > 0 ? this.currentUrl.split('/home')[0] + shareUrl : this.currentUrl;
   }
 }
