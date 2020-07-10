6f9123b415bdedb36313d4f6e3d2b358cfa6cc42
vanshralhan <vansh.ralhan@magnontbwa.com>
2020-07-10 19:49:05 +0530

    feat(knowledge-home): new knowledge home page layout

--- src/app/pages/knowledge/knowledge-home/knowledge-home.component.ts
+++ src/app/pages/knowledge/knowledge-home/knowledge-home.component.ts
@@ -1,15 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewChecked, OnDestroy } from '@angular/core';
 import { GlobalSearchFormSettings, DocumentPageService } from '@pages/shared';
 import { DocumentModel, SearchFilterModel } from '@core/api';
 import { BaseDocumentViewComponent } from '../../shared/abstract-classes/base-document-view.component';
 import { NUXEO_DOC_TYPE } from '@environment/environment';
+import { Router, NavigationStart } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'knowledge-home',
   styleUrls: ['./knowledge-home.component.scss'],
   templateUrl: './knowledge-home.component.html',
 })
-export class KnowledgeHomeComponent extends BaseDocumentViewComponent {
+export class KnowledgeHomeComponent extends BaseDocumentViewComponent implements AfterViewChecked, OnDestroy {
 
   loading: boolean = true;
 
@@ -30,17 +32,47 @@ export class KnowledgeHomeComponent extends BaseDocumentViewComponent {
   };
 
   searchFormSettings: GlobalSearchFormSettings = new GlobalSearchFormSettings({
-    placeholder: 'Search for anything...',
+    placeholder: 'Search',
     enableQueryParams: false,
     skipAggregates: true,
   });
 
-  constructor(protected documentPageService: DocumentPageService) {
+  constructor(
+    protected documentPageService: DocumentPageService,
+    protected router: Router,
+  ) {
     super(documentPageService);
+    this.onRouterChange();
   }
 
   onInit(): void {
     this.setCurrentDocument();
+  }
+
+  ngAfterViewChecked() {
+    const header = document.querySelector('nb-layout-header');
+    if ((typeof (header) !== 'undefined' && header !== null) && (this.router.url.includes('/knowledge/'))) {
+      debugger;
+      header.setAttribute('style', 'display:none');
+      header.classList.remove('fixed');
+    }
+  }
+
+  private onRouterChange(): void {
+    this.subscription = this.router.events.pipe(
+      filter(event => event instanceof NavigationStart),
+    ).subscribe((e: NavigationStart) => {
+      if (!e.url.includes('/knowledge/')) {
+        debugger;
+        const header = document.querySelector('nb-layout-header');
+        header.setAttribute('style', 'display:block');
+        header.classList.add('fixed');
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
 }
