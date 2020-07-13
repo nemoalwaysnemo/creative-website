46c068f627ded0c118e764c3f2b14da1455d7740
Dean Wang <dean.wang@tbwa.com>
2020-07-13 10:31:03 +0800

    improvement(home search): code improvement

--- src/app/pages/shared/knowledge-search-form/knowledge-search-form.component.ts
+++ src/app/pages/shared/knowledge-search-form/knowledge-search-form.component.ts
@@ -1,10 +1,9 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Observable, of as observableOf } from 'rxjs';
-import { DocumentModel, SearchResponse } from '@core/api';
+import { DocumentModel } from '@core/api';
 import { DocumentPageService } from '../services/document-page.service';
-import { BaseSearchFormComponent } from '../global-search-form/base-search-form.component';
+import { HomeSearchFormComponent } from '../home-search-form/home-search-form.component';
 import { GlobalSearchFormSettings } from '../global-search-form/global-search-form.interface';
 import { GlobalSearchFormService } from '../global-search-form/global-search-form.service';
 
@@ -14,27 +13,11 @@ import { GlobalSearchFormService } from '../global-search-form/global-search-for
   styleUrls: ['./knowledge-search-form.component.scss'],
 })
 
-export class KnowledgeSearchFormComponent extends BaseSearchFormComponent {
-
-  documents: DocumentModel[] = [];
-
-  hiddeView: boolean = false;
-
-  backgroudUrl: string = '';
-
-  loadingStyle: any = {};
-
-  layout: string = 'suggestion-inline';
+export class KnowledgeSearchFormComponent extends HomeSearchFormComponent {
 
   formSettings: GlobalSearchFormSettings = new GlobalSearchFormSettings({
     source: 'knowledge-search-form',
   });
-
-  @Input() headline: string;
-
-  @Input() subHead: string;
-
-  @Input() redirectUrl: string;
 
   constructor(
     protected router: Router,
@@ -56,34 +39,8 @@ export class KnowledgeSearchFormComponent extends BaseSearchFormComponent {
     event.stopImmediatePropagation();
   }
 
-  show(): void {
-    this.hiddeView = false;
-  }
-
-  hide(): void {
-    this.hiddeView = true;
-  }
-
   getAssetUrl(doc: DocumentModel): string {
     return '';
   }
 
-  private redirectToListPage(queryParams: any = {}): void {
-    this.router.navigate([this.redirectUrl], { queryParamsHandling: 'merge', queryParams });
-  }
-
-  private isSearchManually(res: SearchResponse): boolean {
-    return (res.searchParams.hasKeyword() || res.searchParams.hasFilters()) && res.metadata.event !== 'onSearchParamsInitialized';
-  }
-
-  protected onBeforeSearchEvent(res: SearchResponse): Observable<SearchResponse> {
-    this.loadingStyle = this.isSearchManually(res) ? { 'min-height': '100px' } : {};
-    return observableOf(res);
-  }
-
-  protected onAfterSearchEvent(res: SearchResponse): Observable<SearchResponse> {
-    this.documents = this.isSearchManually(res) ? res.response.entries : [];
-    this.show();
-    return observableOf(res);
-  }
 }
