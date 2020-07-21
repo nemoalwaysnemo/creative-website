7faa6d8716ebdf76931b36152db6177e4900551a
vanshralhan <vansh.ralhan@magnontbwa.com>
2020-07-21 12:10:59 +0530

    feat(seach-dialog): backslash dialog on knowledge search page

--- src/app/pages/shared/global-search-result/knowledge-document-asset-search-result/knowledge-document-asset-search-result.component.ts
+++ src/app/pages/shared/global-search-result/knowledge-document-asset-search-result/knowledge-document-asset-search-result.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef, Type } from '@angular/core';
 import { DocumentModel } from '@core/api';
 import { NUXEO_PATH_INFO, NUXEO_DOC_TYPE } from '@environment/environment';
 import { getAssetModuleType } from '@core/services/helpers';
+import { GLOBAL_DOCUMENT_DIALOG, GlobalDocumentDialogService, GlobalDocumentDialogSettings } from '../../global-document-dialog';
+import { DocumentModelForm } from '../../global-document-form/global-document-form.component';
+
 
 @Component({
   selector: 'knowledge-document-asset-search-result',
@@ -11,35 +14,74 @@ import { getAssetModuleType } from '@core/services/helpers';
 
 export class KnowledgeDocumentAssetSearchResultComponent {
 
-  constructor() { }
+  constructor(private globalDocumentDialogService: GlobalDocumentDialogService) { }
 
-  getAssetUrl(doc: DocumentModel): string {
-    let url = '';
-    if (NUXEO_DOC_TYPE.CREATIVE_IMAGE_VIDEO_AUDIO_TYPES.includes(doc.type)) {
-      url = '/p/creative/asset';
-    } else if (NUXEO_DOC_TYPE.BACKSLASH_ARTICLE_VIDEO_TYPES.includes(doc.type)) {
-      url = '/p/backslash/asset';
-    } else if (NUXEO_DOC_TYPE.INTELLIGENCE_ASSET_TYPE.includes(doc.type)) {
-      url = '/p/intelligence/asset';
-    } else if (NUXEO_DOC_TYPE.DISRUPTION_ASSET_TYPE.includes(doc.type)) {
-      url = '/p/disruption/asset';
-    } else if (NUXEO_DOC_TYPE.INNOVATION_ASSET_TYPE.includes(doc.type)) {
-      if (doc.path.includes('/NEXT/')) {
-        url = '/p/innovation/NEXT/folder/:parentRef/asset';
-      } else if (doc.path.includes('/Things to Steal/')) {
-        url = '/p/innovation/Things to Steal/folder/:parentRef/asset';
-      }
-    } else if (NUXEO_DOC_TYPE.BIZ_DEV_ASSET_TYPE.includes(doc.type)) {
-      if (doc.type === 'App-BizDev-CaseStudy-Asset') {
-        url = '/p/business-development/Case Studies/folder/:parentRef/asset';
-      } else if (doc.type === 'App-BizDev-Thought-Asset') {
-        url = '/p/business-development/Thought Leadership/folder/:parentRef/asset';
-      }
+  dialogMetadata: any = {
+    // formMode: 'edit',
+    enableEdit: false,
+    enableDeletion: false,
+    moreInfo: true,
+  };
+
+  getDialogSettings(type: string): GlobalDocumentDialogSettings {
+    const components: Type<DocumentModelForm>[] = [];
+    switch (type) {
+      case 'Backslash':
+        components.push(GLOBAL_DOCUMENT_DIALOG.PREIVEW_BACKSLASH_HOME_ASSET);
+        break;
+      case 'Disruption':
+        components.push(GLOBAL_DOCUMENT_DIALOG.PREIVEW_RELATED_DISRUPTION_ASSET);
+        break;
+      case 'Intelligence':
+        components.push(GLOBAL_DOCUMENT_DIALOG.PREIVEW_RELATED_DISRUPTION_ASSET);
+        break;
+      case 'Innovation':
+        components.push(GLOBAL_DOCUMENT_DIALOG.PREIVEW_RELATED_DISRUPTION_ASSET);
+        break;
+      case 'Creative':
+        components.push(GLOBAL_DOCUMENT_DIALOG.PREIVEW_RELATED_DISRUPTION_ASSET);
+        break;
+      case 'Business Development':
+        components.push(GLOBAL_DOCUMENT_DIALOG.PREIVEW_RELATED_DISRUPTION_ASSET);
+        break;
+      default:
+        break;
     }
-    return url.replace(':parentRef', doc.parentRef);
+    return new GlobalDocumentDialogSettings({ components });
   }
+
+  private defaultSettings: any = {
+    columns: {
+      title: {
+        title: 'Title',
+        sort: false,
+      },
+    },
+  };
 
   getAssetType(doc: DocumentModel): string {
     return getAssetModuleType(doc);
+  }
+
+  openDialog(dialog: TemplateRef<any>) {
+    this.globalDocumentDialogService.open(dialog);
+  }
+
+  getTitle(doc: DocumentModel): string {
+    let title = '';
+    if (NUXEO_DOC_TYPE.CREATIVE_IMAGE_VIDEO_AUDIO_TYPES.includes(doc.type)) {
+      title = 'Creative';
+    } else if (NUXEO_DOC_TYPE.BACKSLASH_ARTICLE_VIDEO_TYPES.includes(doc.type)) {
+      title = 'Backslash';
+    } else if (NUXEO_DOC_TYPE.INTELLIGENCE_ASSET_TYPE.includes(doc.type)) {
+      title = 'Intelligence';
+    } else if (NUXEO_DOC_TYPE.DISRUPTION_ASSET_TYPE.includes(doc.type)) {
+      title = 'Disruption';
+    } else if (NUXEO_DOC_TYPE.INNOVATION_ASSET_TYPE.includes(doc.type)) {
+      title = 'Innovation';
+    } else if (NUXEO_DOC_TYPE.BIZ_DEV_ASSET_TYPE.includes(doc.type)) {
+      title = 'Business Development';
+    }
+    return title;
   }
 }
