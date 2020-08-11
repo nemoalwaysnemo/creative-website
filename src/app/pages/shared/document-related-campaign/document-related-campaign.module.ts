7b4d70e75a444a9fe95c5a4991afc5e8d0118f27
vanshralhan <vansh.ralhan@magnontbwa.com>
2020-08-11 15:58:06 +0530

    feat(campaign-popup): creative asset page same campaign assets popup

--- src/app/pages/shared/document-related-campaign/document-related-campaign.module.ts
+++ src/app/pages/shared/document-related-campaign/document-related-campaign.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { ThemeModule } from '@theme/theme.module';
 import { DragScrollModule } from 'ngx-drag-scroll';
 import { DocumentRelatedCampaignComponent } from './document-related-campaign.component';
+import { GlobalDocumentDialogModule } from '../global-document-dialog/global-document-dialog.module';
 
 @NgModule({
   imports: [
     ThemeModule,
     CommonModule,
     DragScrollModule,
+    GlobalDocumentDialogModule,
   ],
   declarations: [
     DocumentRelatedCampaignComponent,
