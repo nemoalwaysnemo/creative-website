148b8548ea716066b148a2e95dba094fe828838a
nemo.yan <nemo.yan@tbwa.com>
2020-08-18 10:39:43 +0800

    refactor(backslash): backslash case study
    backslash case study basic structure
    
    re #623

--- src/app/pages/backslash/backslash-case-study/backslash-case-study.component.ts
+++ src/app/pages/backslash/backslash-case-study/backslash-case-study.component.ts
@@ -1,16 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { parseTabRoute } from '@core/services/helpers';
 import { TAB_CONFIG } from '../backslash-tab-config';
-import { GlobalDocumentViewComponent, DocumentPageService } from '@pages/shared';
 import { NUXEO_PATH_INFO, NUXEO_DOC_TYPE } from '@environment/environment';
-
+import { Subject, timer } from 'rxjs';
+import { DocumentModel, GlobalSearchParams, SearchFilterModel, NuxeoRequestOptions } from '@core/api';
+import { DocumentPageService, GlobalDocumentViewComponent, GlobalSearchFormSettings } from '@pages/shared';
 @Component({
   selector: 'backslash-case-study',
   styleUrls: ['./backslash-case-study.component.scss'],
   templateUrl: './backslash-case-study.component.html',
 })
-export class BackslashCaseStudyComponent extends GlobalDocumentViewComponent {
+export class BackslashCaseStudyComponent extends GlobalDocumentViewComponent implements OnInit {
 
   tabs: any[] = parseTabRoute(TAB_CONFIG);
 
@@ -26,13 +27,73 @@ export class BackslashCaseStudyComponent extends GlobalDocumentViewComponent {
     this.subscription.add(subscription);
   }
 
+  baseParams$: Subject<any> = new Subject<any>();
+
+  filters: SearchFilterModel[] = [
+    new SearchFilterModel({ key: 'the_loupe_main_brand_agg', placeholder: 'Brand' }),
+    new SearchFilterModel({ key: 'app_edges_industry_agg', placeholder: 'Industry', iteration: true }),
+    new SearchFilterModel({ key: 'the_loupe_main_country_agg', placeholder: 'Country', iteration: true }),
+    new SearchFilterModel({ key: 'the_loupe_main_agency_agg', placeholder: 'Agency' }),
+  ];
+
+  searchFormSettings: GlobalSearchFormSettings = new GlobalSearchFormSettings({
+    enableQueryParams: true,
+  });
+
+  beforeSearch: Function = (searchParams: GlobalSearchParams, opts: NuxeoRequestOptions): { searchParams: GlobalSearchParams, opts: NuxeoRequestOptions } => {
+    if (searchParams.hasKeyword()) {
+      searchParams = this.buildSearchAssetsParams(searchParams);
+    }
+    return { searchParams, opts };
+  }
+
+
+  ngOnInit(): void {
+    const subscription = this.searchCurrentDocument(this.getCurrentDocumentSearchParams()).subscribe();
+    this.subscription.add(subscription);
+  }
+
+  // get all matched assets and their parent folders
+  protected buildSearchAssetsParams(searchParams: GlobalSearchParams): GlobalSearchParams {
+    const params: any = {
+      currentPageIndex: searchParams.getSettings('append') ? searchParams.providerParams.currentPageIndex : 0,
+      pageSize: searchParams.getSettings('append') ? searchParams.providerParams.pageSize : GlobalSearchParams.PageSize,
+      ecm_mixinType_not_in: '',
+      ecm_fulltext: searchParams.providerParams.ecm_fulltext_wildcard,
+      ecm_path: NUXEO_PATH_INFO.BIZ_DEV_CASE_STUDIES_FOLDER_PATH,
+      ecm_primaryType: NUXEO_DOC_TYPE.BIZ_DEV_CASE_STUDIES_SUB_FOLDER_TYPE,
+    };
+    return searchParams.setParams(params);
+  }
+
+  protected setCurrentDocument(doc: DocumentModel): void {
+    super.setCurrentDocument(doc);
+    if (doc) {
+      timer(0).subscribe(() => { this.baseParams$.next(this.buildDefaultAssetsParams(doc)); });
+    }
+  }
+
   protected getCurrentDocumentSearchParams(): any {
     return {
       pageSize: 1,
       currentPageIndex: 0,
-      ecm_path_eq: NUXEO_PATH_INFO.BACKSLASH_RESOURCES_FOLDER_PATH,
-      ecm_primaryType: NUXEO_DOC_TYPE.BACKSLASH_ASSET_FOLDER_TYPE,
+      ecm_path_eq: NUXEO_PATH_INFO.BIZ_DEV_CASE_STUDIES_FOLDER_PATH,
+      ecm_primaryType: NUXEO_DOC_TYPE.BIZ_DEV_CASE_STUDIES_BASE_FOLDER_TYPE,
     };
+  }
+
+  protected buildDefaultAssetsParams(doc: DocumentModel): GlobalSearchParams {
+    const params: any = {
+      currentPageIndex: 0,
+      ecm_fulltext: '',
+      ecm_mixinType_not_in: '',
+      ecm_path: NUXEO_PATH_INFO.BIZ_DEV_CASE_STUDIES_FOLDER_PATH,
+      ecm_primaryType: NUXEO_DOC_TYPE.BIZ_DEV_CASE_STUDIES_SUB_FOLDER_TYPE,
+    };
+    if (doc) {
+      params['ecm_parentId'] = doc.uid;
+    }
+    return new GlobalSearchParams(params);
   }
 
 }
--- src/app/pages/backslash/backslash-case-study/backslash-case-study.module.ts
+++ src/app/pages/backslash/backslash-case-study/backslash-case-study.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { ThemeModule } from '@theme/theme.module';
 import { BackslashCaseStudyComponent } from './backslash-case-study.component';
 import { SharedModule } from '@pages/shared/shared.module';
-
+import { GlobalSearchFormModule, GlobalSearchResultModule } from '../../shared';
+import { BackslashFormButtonModule } from '../backslash-form-button/backslash-form-button.module';
 @NgModule({
   imports: [
     ThemeModule,
     SharedModule,
+    GlobalSearchFormModule,
+    GlobalSearchResultModule,
+    BackslashFormButtonModule,
   ],
   declarations: [
     BackslashCaseStudyComponent,
