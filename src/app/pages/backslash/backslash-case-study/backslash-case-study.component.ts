946b8be4d92ae4498a9db6ff55721a4b1bfc4cc5
nemo.yan <nemo.yan@tbwa.com>
2020-09-02 14:26:00 +0800

    refactor(backslash): case study
    case study refactor

--- src/app/pages/backslash/backslash-case-study/backslash-case-study.component.ts
+++ src/app/pages/backslash/backslash-case-study/backslash-case-study.component.ts
@@ -61,7 +61,7 @@ export class BackslashCaseStudyComponent extends GlobalDocumentViewComponent imp
       ecm_mixinType_not_in: '',
       ecm_fulltext: searchParams.providerParams.ecm_fulltext_wildcard,
       ecm_path: NUXEO_PATH_INFO.BACKSLASH_CASE_STUDIES_FOLDER_PATH,
-      ecm_primaryType: NUXEO_DOC_TYPE.BACKSLASH_CASE_STUDIES_SUB_FOLDER_TYPE,
+      ecm_primaryType: NUXEO_DOC_TYPE.BACKSLASH_CASE_STUDIES_FOLDER_ASSETS,
     };
     return searchParams.setParams(params);
   }
