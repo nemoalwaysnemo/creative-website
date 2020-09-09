af2bce04361cc5dcc22b69b5da1fdd3d2fc0b869
nemo.yan <nemo.yan@tbwa.com>
2020-09-09 15:36:52 +0800

    refactor(project): project 3rd party import

---
+++ src/app/pages/shared/document-creative-project-mgt/document-creative-project-import-request/document-creative-project-import-requests-view/document-creative-project-import-requests-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+import { GlobalDocumentFormComponent } from '../../../global-document-form/global-document-form.component';
+
+
+@Component({
+  selector: 'document-creative-project-import-requests-view',
+  styleUrls: ['../../document-creative-project-mgt.component.scss'],
+  templateUrl: './document-creative-project-import-requests-view.component.html',
+})
+export class DocumentCreativeProjectImportRequestsViewComponent extends GlobalDocumentFormComponent {
+}
