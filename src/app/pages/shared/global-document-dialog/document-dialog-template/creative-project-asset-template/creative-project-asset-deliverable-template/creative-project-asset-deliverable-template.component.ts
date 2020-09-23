53e28bd4113bf26dc485d45876b848267d9080c6
billy.zhang <billy.zhang@tbwa.com>
2020-09-23 09:11:35 +0800

    feat(campaign summary): upload, deliver buttons

--- src/app/pages/shared/global-document-dialog/document-dialog-template/creative-project-asset-template/creative-project-asset-deliverable-template/creative-project-asset-deliverable-template.component.ts
+++ src/app/pages/shared/global-document-dialog/document-dialog-template/creative-project-asset-template/creative-project-asset-deliverable-template/creative-project-asset-deliverable-template.component.ts
@@ -1,5 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, TemplateRef } from '@angular/core';
 import { CreativeProjectAssetBaseTemplateComponent } from '../creative-project-asset-base-template.component';
+import { NbTabComponent } from '@core/nebular/theme/components/tabset/tabset.component';
+import { DocumentModel } from '@core/api';
+import { Subject, timer } from 'rxjs';
+
+export class TabInfo {
+  readonly type: string;
+  readonly tabItem: any;
+  readonly document: DocumentModel;
+  constructor(type: string, item: any, doc: DocumentModel) {
+    this.type = type;
+    this.tabItem = item;
+    this.document = doc;
+  }
+}
 
 @Component({
   selector: 'creative-project-asset-deliverable-template',
@@ -7,10 +21,6 @@ import { CreativeProjectAssetBaseTemplateComponent } from '../creative-project-a
   templateUrl: './creative-project-asset-deliverable-template.component.html',
 })
 export class CreativeProjectAssetDeliverableTemplateComponent extends CreativeProjectAssetBaseTemplateComponent {
-
-  selectedAsset = false;
-  selectedNew = false;
-  selectedDelivery = false;
 
   listViewOptionsAsset: any = {
     deliverPackage: false,
@@ -22,6 +32,38 @@ export class CreativeProjectAssetDeliverableTemplateComponent extends CreativePr
     deliverPackage: true,
   };
 
+  tabItems: any = [
+    {
+      name: 'Assets',
+      layout: 'assets',
+    },
+    {
+      name: 'New Package',
+      layout: 'packageForm',
+    },
+    {
+      name: 'Delivery Package',
+      layout: 'packageList',
+    },
+  ];
+
+  tabInfo$ = new Subject<TabInfo>();
+
+  currentTab: any;
+
+  templateRef: TemplateRef<any>;
+
+  setDocument(doc: DocumentModel): void {
+    if (doc) {
+      this.document = doc;
+      timer(0).subscribe(() => { this.tabInfo$.next(new TabInfo('docChanged', this.currentTab, doc)); });
+    }
+  }
+
+  @ViewChild('assetItemView', { static: true }) private assetItemView: TemplateRef<any>;
+  @ViewChild('newPackageItemView', { static: true }) private newPackageItemView: TemplateRef<any>;
+  @ViewChild('packageListItemView', { static: true }) private packageListItemView: TemplateRef<any>;
+
   onResponse(refresh: boolean): void {
     if (refresh) {
       this.listViewOptionsPackage = {
@@ -32,19 +74,27 @@ export class CreativeProjectAssetDeliverableTemplateComponent extends CreativePr
     }
   }
 
-  protected onInit(): void {
-    console.log(44444, this.settings);
-    const selectedTitle: string = this.settings.selectedTab;
-    switch (selectedTitle) {
-      case 'Asset':
-        this.selectedAsset = true;
-        break;
-      case 'New Package':
-        this.selectedNew = true;
-        break;
-      case 'Delivery Package':
-        this.selectedDelivery = true;
-        break;
+  onInit() {
+    const selectedTitle: string = this.settings.selectedTab ? this.settings.selectedTab : 'Assets';
+    this.currentTab = this.getTabItem(selectedTitle);
+  }
+
+  onChangTab(tab: NbTabComponent): void {
+    const info = this.getTabItem(tab.tabTitle);
+    if (info.name === 'Assets') {
+      this.templateRef = this.assetItemView;
+    } else if (info.name === 'New Package') {
+      this.templateRef = this.newPackageItemView;
+    } else if (info.name === 'Delivery Package') {
+      this.templateRef = this.packageListItemView;
     }
+    if (tab.tabTitle !== this.currentTab.name) {
+      this.tabInfo$.next(new TabInfo('tabChanged', info, this.document));
+    }
+    this.currentTab = info;
+  }
+
+  private getTabItem(tabTitle: string): any {
+    return this.tabItems.filter((x) => tabTitle === x.name).shift();
   }
 }
