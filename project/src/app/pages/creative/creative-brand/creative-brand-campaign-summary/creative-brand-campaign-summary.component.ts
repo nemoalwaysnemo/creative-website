ea7a36d06678fcd549a575587b42df552785449a
Dean Wang <dean.wang@tbwa.com>
2020-11-16 14:39:37 +0800

    refactor(creative mgt): code improvement

--- project/src/app/pages/creative/creative-brand/creative-brand-campaign-summary/creative-brand-campaign-summary.component.ts
+++ project/src/app/pages/creative/creative-brand/creative-brand-campaign-summary/creative-brand-campaign-summary.component.ts
@@ -3,8 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { Subject, timer } from 'rxjs';
 import { DocumentModel, SearchFilterModel, NuxeoSearchConstants } from '@core/api';
 import { GLOBAL_DOCUMENT_FORM } from '../../../shared/global-document-form';
-import { ListSearchRowCustomDialogComponent } from '../../../shared/list-search-form';
-import { ListSearchRowForUrComponent } from '../../../shared/list-search-form-for-ur';
+import { ListSearchFormComponent, ListSearchRowCustomDialogComponent } from '../../../shared/list-search-form';
 import { ListSearchRowCustomViewSettings } from '../../../shared/list-search-form/list-search-form.interface';
 import { DocumentPageService, GlobalDocumentViewComponent, GlobalSearchFormSettings, DocumentListViewItem } from '@pages/shared';
 import { GLOBAL_DOCUMENT_DIALOG, GlobalDocumentDialogSettings, GlobalDocumentDialogService } from '../../../shared/global-document-dialog';
@@ -118,7 +117,7 @@ export class CreativeBrandCampaignSummaryComponent extends GlobalDocumentViewCom
             ],
           }),
         }),
-        renderComponent: ListSearchRowForUrComponent,
+        renderComponent: ListSearchRowCustomDialogComponent,
       },
     },
   };
