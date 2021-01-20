24d64a82d623fe8c3732d4f1b05292c575625001
Nemo.Yan <nemo.yan@tbwa.com>
2021-01-20 02:55:24 -0800

    refactor(program alumni): alumni html and css

--- project/src/app/pages/learning/learning-alumni/learning-alumni.component.ts
+++ project/src/app/pages/learning/learning-alumni/learning-alumni.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { DocumentPageService } from '@pages/shared';
 import { BaseDocumentViewComponent } from '../../shared/abstract-classes/base-document-view.component';
-
+import { NUXEO_PATH_INFO, NUXEO_DOC_TYPE } from '@environment/environment';
+import { DocumentModel, GlobalSearchParams, NuxeoPagination } from '@core/api';
+import { TAB_CONFIG } from '../learning-tab-config';
 @Component({
   selector: 'learning-alumni',
   styleUrls: ['./learning-alumni.component.scss'],
@@ -9,12 +11,51 @@ import { BaseDocumentViewComponent } from '../../shared/abstract-classes/base-do
 })
 export class LearningAlumniComponent extends BaseDocumentViewComponent {
 
+
+  loading: boolean = true;
+
+  enableFeature: boolean = true;
+
+  documents: DocumentModel[] = [];
+
+  private params: any = {
+    pageSize: 12,
+    currentPageIndex: 0,
+    ecm_primaryType: NUXEO_DOC_TYPE.DISRUPTION_X_TYPE,
+    ecm_path: NUXEO_PATH_INFO.DISRUPTION_X_FOLDER_PATH,
+  };
+
+  private tabs: any[] = TAB_CONFIG;
+
   constructor(protected documentPageService: DocumentPageService) {
     super(documentPageService);
   }
 
   onInit(): void {
-    this.setCurrentDocument();
+    this.search(this.params);
   }
 
+  getAssetUrl(doc: DocumentModel): string {
+    let url: string = '/p/learning';
+    for (const tab of this.tabs) {
+      if (doc.path.includes(tab.title)) {
+        url = tab.route;
+      }
+    }
+    return url;
+  }
+
+  goToLink(doc: DocumentModel): void {
+    this.documentPageService.goToExternalLink(doc);
+  }
+
+  private search(params: {}): void {
+    const subscription = this.documentPageService.advanceRequest(new GlobalSearchParams(params))
+      .subscribe((res: NuxeoPagination) => {
+        this.documents = res.entries;
+        this.loading = false;
+      });
+    this.subscription.add(subscription);
+  }
 }
+
