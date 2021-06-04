9fdd7a353128713f2668a761622a4879d99b82cd
Nemo.Yan <nemo.yan@tbwa.com>
2021-06-04 17:17:48 +0800

    refactor(progect mgt): import asset and bulk import refactor

--- project/src/app/pages/shared/document-creative-project-mgt/document-creative-project-import-asset-page/document-creative-project-import-asset.module.ts
+++ project/src/app/pages/shared/document-creative-project-mgt/document-creative-project-import-asset-page/document-creative-project-import-asset.module.ts
@@ -10,6 +10,7 @@ import { DocumentCreativeProjectImportAssetComponent } from './document-creative
 import { DocumentNewPosterButtonModule } from '../../document-new-poster-button/document-new-poster-button.module';
 import { DocumentCreativeProjectImportAssetHomeComponent } from './document-creative-project-import-asset-home/document-creative-project-import-asset-home.component';
 import { DocumentCreativeProjectImportAssetFormComponent } from './document-creative-project-import-asset-form/document-creative-project-import-asset-form.component';
+import { DocumentBulkImportModule } from '../../document-bulk-import/document-bulk-import.module';
 @NgModule({
   imports: [
     CommonModule,
@@ -20,6 +21,7 @@ import { DocumentCreativeProjectImportAssetFormComponent } from './document-crea
     ListSearchFormInDialogModule,
     DocumentNewPosterButtonModule,
     DocumentCreativeProjectNavigationModule,
+    DocumentBulkImportModule,
   ],
   declarations: [
     DocumentCreativeProjectImportAssetComponent,
