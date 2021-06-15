a88e9fbc00b10a220564eb6bfea63b1022542837
Nemo.Yan <nemo.yan@tbwa.com>
2021-06-15 14:51:50 +0800

    refactor(3rd party import): refactor move to libary functions

--- project/src/app/pages/shared/document-creative-project-mgt/document-creative-project-3rd-party-import-page/document-creative-project-3rd-party-import-review-page/document-creative-project-3rd-party-import-review-page.component.ts
+++ project/src/app/pages/shared/document-creative-project-mgt/document-creative-project-3rd-party-import-page/document-creative-project-3rd-party-import-review-page/document-creative-project-3rd-party-import-review-page.component.ts
@@ -1,17 +1,14 @@
-import { Component, OnChanges } from '@angular/core';
-import { DocumentModel, UserModel, NuxeoRequestOptions, NuxeoAutomations } from '@core/api';
-import { DocumentFormSettings } from '../../../document-form/document-form.interface';
-import { GlobalDocumentFormComponent } from '../../../global-document-form/global-document-form.component';
-import { DynamicSuggestionModel, DynamicInputModel, DynamicTextAreaModel } from '@core/custom';
-import { SuggestionSettings } from '../../../document-form-extension';
+
+import { Component } from '@angular/core';
+import { DocumentModel, NuxeoAutomations } from '@core/api';
 import { NUXEO_DOC_TYPE } from '@environment/environment';
 import { ListSearchRowCustomViewSettings } from '../../../list-search-form/list-search-form.interface';
 import { ListSearchRowCustomViewComponent } from '../../../list-search-form-in-dialog/list-search-row-custom-view-component';
 import { DocumentListViewItem } from '../../../document-list-view/document-list-view.interface';
 import { GlobalSearchFormSettings } from '../../../global-search-form/global-search-form.interface';
 import { DocumentCreativeProjectMgtBaseComponent } from '../../document-creative-project-mgt-base.component';
+import { Observable, Subject, timer } from 'rxjs';
 import { CreativeProjectMgtSettings } from '../../document-creative-project-mgt.interface';
-import { of as observableOf, Observable, Subject, timer } from 'rxjs';
 @Component({
   selector: 'creative-brand-project-3rd-party-import-review',
   templateUrl: './document-creative-project-3rd-party-import-review-page.component.html',
@@ -25,6 +22,8 @@ export class DocumentCreativeProject3rdPartyImportReviewComponent extends Docume
 
   baseParams$: Subject<any> = new Subject<any>();
 
+  assetsMovingQueue: any[] = [];
+
   searchFormSettings: GlobalSearchFormSettings = new GlobalSearchFormSettings({
     schemas: ['dublincore', 'The_Loupe_Main', 'The_Loupe_Delivery', 'collection'],
     source: 'document-creative-project-import-requests-review',
@@ -34,6 +33,8 @@ export class DocumentCreativeProject3rdPartyImportReviewComponent extends Docume
   listViewSettings: any = {
     hideHeader: false,
     hideSubHeader: true,
+    showCheckbox: true,
+    selectMode: 'multi',
     columns: {
       thumbnail: {
         sort: false,
@@ -49,16 +50,33 @@ export class DocumentCreativeProject3rdPartyImportReviewComponent extends Docume
         title: 'title',
         renderComponent: ListSearchRowCustomViewComponent,
       },
+      production_date: {
+        sort: false,
+        title: 'Production Date',
+        renderComponent: ListSearchRowCustomViewComponent,
+      },
+      asset_countries: {
+        sort: false,
+        title: 'Asset Countries',
+      },
+      job_number: {
+        sort: false,
+        title: 'Job Number',
+      },
     },
   };
 
   listViewBuilder: (docs: DocumentModel[]) => any = (docs: DocumentModel[]) => {
     const items = [];
+
     for (const doc of docs) {
       items.push(new DocumentListViewItem({
         info: doc,
         thumbnail: doc,
         title: doc.title,
+        production_date: doc.properties['dc:created'],
+        asset_countries: doc.properties['The_Loupe_Main:country'].join(','),
+        job_number: doc.properties['The_Loupe_Main:jobnumber'],
       }));
     }
     return items;
@@ -85,14 +103,35 @@ export class DocumentCreativeProject3rdPartyImportReviewComponent extends Docume
   protected onInit(): void {
   }
 
-  protected acceptAsset(request_id: any, assets_ids: any): Observable<any> {
-    return this.documentPageService.operation(NuxeoAutomations.MoveAssetToLibrary, { request_id, assets_ids});
+  protected acceptAsset(request_id: string, assets_ids: string): Observable<any> {
+    return this.documentPageService.operation(NuxeoAutomations.MoveAssetToLibrary, { request_id, assets_ids });
   }
 
-
-  onSelected(row: any): void {
-    this.acceptAsset(this.requestDocument.uid, row.data.info.uid).subscribe((res) => {
+  moveAssets(): void {
+    this.assetsMovingQueue.forEach((row) => {
+      if (row.isSelected) {
+        this.acceptAsset(this.requestDocument.uid, row.data.info.uid).subscribe(_ => {
+          this.refresh();
+        });
+      }
     });
   }
 
+  onSelected(row: any): void {
+    if (row.isSelected) {
+      this.assetsMovingQueue.push(row);
+    }
   }
+
+  private refresh(): void {
+    this.triggerChangeView('3rd-import-request-review', 'view',
+      new CreativeProjectMgtSettings({
+        document: this.requestDocument,
+        project: this.templateSettings.project,
+        homeTemplate: 'creative-project-mgt-template',
+        homePage: '3rd-import-Page',
+        homeView: '3rd-import-home-view',
+        showMessageBeforeSave: false,
+      }));
+  }
+}
